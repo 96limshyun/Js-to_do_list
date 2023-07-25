317f2f6371959e4cb930b1f8915ccd71718ee0da
limshyun <96limshyun@gmail.com>
2023-07-25 23:10:36 +0900

    북마크 기능 작업중

---
+++ Web_Front/js/search.js
@@ -0,0 +1,16 @@
+const searchInput = document.getElementById("search-input");
+
+const showSearchResult = () => {
+    let searchWorld = searchInput.value;
+    window.location.href = `https://www.google.com/search?q=${searchWorld}`
+    searchWorld = "";
+}
+
+const enterKey = (event) => {
+    if (event.code === "Enter") {
+        showSearchResult();
+    }
+}
+searchInput.addEventListener("keypress", (event) => {
+enterKey(event);
+})
