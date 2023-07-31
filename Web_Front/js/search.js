600385d05f92615eb35e02ed1c9c549170a423d4
limshyun <96limshyun@gmail.com>
2023-07-31 17:37:01 +0900

    북마크 기능 작업중2

--- Web_Front/js/search.js
+++ Web_Front/js/search.js
@@ -1,16 +1,18 @@
-const searchInput = document.getElementById("search-input");
+(function(){
+    const searchInput = document.getElementById("search-input");
 
-const showSearchResult = () => {
-    let searchWorld = searchInput.value;
-    window.location.href = `https://www.google.com/search?q=${searchWorld}`
-    searchWorld = "";
-}
-
-const enterKey = (event) => {
-    if (event.code === "Enter") {
-        showSearchResult();
+    const showSearchResult = () => {
+        let searchWorld = searchInput.value;
+        window.location.href = `https://www.google.com/search?q=${searchWorld}`
+        searchWorld = "";
     }
-}
-searchInput.addEventListener("keypress", (event) => {
-enterKey(event);
-})
+    
+    const enterKey = (event) => {
+        if (event.code === "Enter") {
+            showSearchResult();
+        }
+    }
+    searchInput.addEventListener("keypress", (event) => {
+    enterKey(event);
+    })
+})();
